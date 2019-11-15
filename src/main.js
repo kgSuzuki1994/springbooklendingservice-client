c910e747ee1c723da864b4c2536e43093ba18362
kgSuzuki1994 <takuti0511@gmail.com>
2019-11-15 13:33:19 +0900

    [add] ElementUIの導入

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+import ElementUI from "element-ui";
+import locale from "element-ui/lib/locale/lang/ja";
+import "element-ui/theme-chalk/index.css";
+
+Vue.config.productionTip = false;
+Vue.use(ElementUI, { locale });
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
