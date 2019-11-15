04feffb70745d59be70dafd4506dc6d9afe051a9
kgSuzuki1994 <takuti0511@gmail.com>
2019-11-15 13:49:15 +0900

    [add] BookListページの追加
    書籍一覧を表示するためのページを追加

---
+++ src/views/BookList.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-row>
+    <el-col :span="24">
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span>書籍一覧</span>
+        </div>
+        <div>...</div>
+      </el-card>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: "BookList"
+};
+</script>
+
+<style scoped></style>
