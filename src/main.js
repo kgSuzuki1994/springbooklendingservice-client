5d573631cdcfbbd00268a1d94ea54c0e742e6232
kgSuzuki1994 <takuti0511@gmail.com>
2019-11-15 13:47:32 +0900

    [fix] imortの記載を修正
    element-uiの記載に誤記があったので修正

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import store from "./store";
 
 import ElementUI from "element-ui";
 import locale from "element-ui/lib/locale/lang/ja";
-import "element-ui/theme-chalk/index.css";
+import "element-ui/lib/theme-chalk/index.css";
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI, { locale });
