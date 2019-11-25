6145f8ed7b732e0c35d5c7e7ff240257189c405b
kgSuzuki1994 <takuti0511@gmail.com>
2019-11-25 10:34:33 +0900

    [fix] #7 SigninとSignupの格納先をviewsファイルに変更

---
+++ src/views/Signin.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="signin">
+    <h2>Sign in</h2>
+    <input type="text" placeholder="Username" v-model="username" />
+    <input type="password" placeholder="Password" v-model="password" />
+    <button>Signin</button>
+    <p>
+      You don't have an account?
+      <router-link to="/signup">create account now!!</router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Signin",
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  methods: {}
+};
+</script>
