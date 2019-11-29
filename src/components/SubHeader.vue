b2938b322cb5788ebecf36e6facd26be03c0ec70
kgSuzuki1994 <takuti0511@gmail.com>
2019-11-29 13:40:18 +0900

    [add] ログアウトボタンの追加

---
+++ src/components/SubHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-button type="text" class="signout-label" @click="signout"
+    >Signout</el-button
+  >
+</template>
+
+<script>
+import firebase from "firebase";
+
+export default {
+  name: "SubHeader",
+  methods: {
+    signout: function() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.$router.push("/signout");
+        });
+    }
+  }
+};
+</script>
+
+<style scoped></style>
