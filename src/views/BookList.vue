14bd42d4fb2c38de7a5fda9486ea2e1f3ee35cff
kgSuzuki1994 <takuti0511@gmail.com>
2019-11-29 13:59:28 +0900

    [add] SubHeaderを表示

--- src/views/BookList.vue
+++ src/views/BookList.vue
@@ -1,5 +1,6 @@
 <template>
   <el-row>
+    <sub-header />
     <el-col :span="24">
       <el-card class="box-card">
         <div slot="header" class="clearfix">
@@ -11,11 +12,16 @@
           <el-table-column prop="title" label="書籍名" width="450" />
           <el-table-column prop="status" label="貸し出し状況" width="150" />
           <el-table-column prop="person" label="貸出人" width="250" />
-          <el-table-column prop="operation" label="内容更新" width="200" align="left">
+          <el-table-column
+            prop="operation"
+            label="内容更新"
+            width="200"
+            align="left"
+          >
             <template slot-scope="scope">
               <router-link
                 @click.native="editBookInfo()"
-                :to="{name: 'editbook', params: {id: scope.row.id}}"
+                :to="{ name: 'editbook', params: { id: scope.row.id } }"
               >
                 <el-button type="primary" icon="el-icon-edit"></el-button>
               </router-link>
@@ -29,9 +35,11 @@
 
 <script>
 import axios from "axios";
+import SubHeader from "../components/SubHeader";
 
 export default {
   name: "BookList",
+  components: { SubHeader },
   data() {
     return {
       bookLists: []
