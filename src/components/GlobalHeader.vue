2339b3afad3e54709d1eb9668b6aa19b7f748d74
kgSuzuki1994 <takuti0511@gmail.com>
2019-11-29 13:43:43 +0900

    [add] GlobalHeaderを追加

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="global-header">
+    <span class="global-header-label">Welcome to Our Library!!</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GlobalHeader"
+};
+</script>
+
+<style scoped lang="scss">
+.global-header {
+  background-color: $HEADER_BACKGROUND;
+  height: 6.5vh;
+}
+.global-header-label {
+  color: $HEADER_LABEL;
+  font-weight: bold;
+  line-height: 6.5vh;
+}
+</style>
