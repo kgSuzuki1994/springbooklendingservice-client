a34a3d1b7e858d4bfd8359406f0b6a13a062cc39
kgSuzuki1994 <takuti0511@gmail.com>
2019-11-29 13:35:24 +0900

    [fix] 認証前の処理の動作を修正

--- src/router/index.js
+++ src/router/index.js
@@ -5,60 +5,62 @@ import firebase from "firebase";
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: Home
-  },
-  {
-    path: "/signup",
-    name: "Signup",
-    component: () =>
-      import(/* webpackChunkName: "signup" */ "../views/Signup.vue")
-  },
-  {
-    path: "/signin",
-    name: "Signin",
-    component: () =>
-      import(/* webpackChunkName: "signin" */ "../views/Signin.vue")
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  },
-  {
-    path: "/booklist",
-    name: "booklist",
-    component: () =>
-      import(/* webpackChunkName: "booklist" */ "../views/BookList.vue")
-  },
-  {
-    path: "/addbook",
-    name: "addbook",
-    component: () =>
-      import(/* webpackChunkName: "addbook" */ "../views/AddBook.vue")
-  },
-  {
-    path: "/booklist/editbook/:id",
-    name: "editbook",
-    component: () =>
-      import(/* webpackChunkName: "addbook" */ "../views/editbook.vue"),
-    props: route => ({
-      value: Number(route.params.id)
-    })
-  }
-];
-
-const router = new VueRouter({
+let router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: "/signup",
+      name: "Signup",
+      component: () =>
+        import(/* webpackChunkName: "signup" */ "../views/Signup.vue")
+    },
+    {
+      path: "/signin",
+      name: "Signin",
+      component: () =>
+        import(/* webpackChunkName: "signin" */ "../views/Signin.vue")
+    },
+    {
+      path: "/about",
+      name: "about",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "../views/About.vue")
+    },
+    {
+      path: "/booklist",
+      name: "booklist",
+      component: () =>
+        import(/* webpackChunkName: "booklist" */ "../views/BookList.vue"),
+      meta: { requiresAuth: true }
+    },
+    {
+      path: "/addbook",
+      name: "addbook",
+      component: () =>
+        import(/* webpackChunkName: "addbook" */ "../views/AddBook.vue"),
+      meta: { requiresAuth: true }
+    },
+    {
+      path: "/booklist/editbook/:id",
+      name: "editbook",
+      component: () =>
+        import(/* webpackChunkName: "addbook" */ "../views/editbook.vue"),
+      meta: { requiresAuth: true },
+      props: route => ({
+        value: Number(route.params.id)
+      })
+    }
+  ]
 });
 
 router.beforeEach((to, from, next) => {
